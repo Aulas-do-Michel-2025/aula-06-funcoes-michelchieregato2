1c0c363c3c50677071cbed0fb1a067cd4a50eb18
michelchieregato2 <michel.chieregato@medway.com.br>
2025-06-10 09:13:58 -0300

    Update exercicio_2.py

--- exercicio_2.py
+++ exercicio_2.py
@@ -8,3 +8,10 @@ Exemplo de uso:
 >>> print(criar_organismo(10, 'HIV', 1000))
 >>> {"id": 10, "nome": "HIV", 1000}
 """
+
+def criar_organismo(id, nome, tamanho_do_genoma):
+    return {
+        "id": id,
+        "nome": nome,
+        "tamanho_do_genoma": tamanho_do_genoma,
+    }
