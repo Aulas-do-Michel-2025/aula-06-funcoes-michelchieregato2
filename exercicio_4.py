1b8cd21157bf97e1f2cd15aadfab854f3757102e
michelchieregato2 <michel.chieregato@medway.com.br>
2025-06-10 09:14:30 -0300

    Update exercicio_4.py

--- exercicio_4.py
+++ exercicio_4.py
@@ -20,5 +20,7 @@ def verificar_se_eh_primo(numero):
 
 def filtrar_lista_por_numeros_primos(lista):
     lista_filtrada = []
-    # Completar funcao aqui!
+    for numero in lista:
+        if verificar_se_eh_primo(numero):
+            lista_filtrada.append(numero)
     return lista_filtrada
